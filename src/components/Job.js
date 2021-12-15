8e393ec1c850e32551f3e62fd149ad2e62578ea6
Christiaan <christiaanvaneijnsbergen@gmail.com>
2021-12-15 12:45:09 +0100

    Work update

--- src/components/Job.js
+++ src/components/Job.js
@@ -2,29 +2,27 @@ import React from 'react'
 import './Job.scss'
 
 const Job = ({ job }) => {
-    return (
-        <div className="job">
-            <h3 className="job__title">
-                
-                {job.title} @&nbsp;
-                <a
-                    href={job.url}
-                    className="job__link"
-                    rel="noreferrer"
-                    target="_blank"
-                    title={job.company}
-                    alt={job.company}
-                >
-                    {job.company}
-                </a>
-                
-            </h3>
+  return (
+    <div className="job">
+      <h3 className="job__title">
 
-            <span className="job__range">{job.startDate} - {job.endDate}</span>
+        {job.title} @&nbsp;
+        <a
+          href={job.url}
+          className="job__link"
+          rel="noreferrer"
+          target="_blank"
+          title={job.company}
+          alt={job.company}
+        >
+          {job.company}
+        </a>
 
+      </h3>
 
-        </div>
-    )
+      <span className="job__range">{job.startDate} - {job.endDate}</span>
+    </div>
+  )
 }
 
 export default Job
--- src/lib/data.js
+++ src/lib/data.js
@@ -1,129 +1,139 @@
 const projects = [
-    {
-        id: 0,
-        name: 'Personal website',
-        description: 'Consectetur ullamco qui Lorem ad culpa sit do cupidatat nostrud duis ad et. Exercitation cupidatat consequat deserunt amet commodo sit velit labore amet mollit officia. Esse et quis irure sunt voluptate quis. Exercitation sunt eiusmod aute cillum proident sunt cillum. Labore adipisicing deserunt cillum dolor anim cupidatat in velit quis sunt esse id.\r\n',
-        preview: 'https://cve-personal.netlify.app/',
-        github: 'https://github.com/CVE-078/personal-website',
-        type: 'Website',
-        stack: ['React', 'SCSS', 'BEM'],
-        picture: '/assets/v7RfA1q.webp'
-    },
-    {
-        id: 1,
-        name: 'Airbnb Clone',
-        description: 'Dolore amet occaecat mollit mollit dolore. Et do anim est labore voluptate dolore nulla proident. Ipsum anim esse magna dolore quis ullamco consequat est ullamco nulla. Non eu consequat incididunt ut mollit pariatur irure officia. Eu commodo ad aliqua fugiat mollit amet.\r\n',
-        preview: 'https://airbnb-clone-react.vercel.app/',
-        github: 'https://github.com/CVE-078/airbnb-clone-react',
-        type: 'Clone',
-        stack: ['Next.js', 'Tailwind CSS', 'API'],
-        picture: '/assets/Wg58djZ.webp'
-    },
-    {
-        id: 2,
-        name: 'F1 Game Leaderboard',
-        description: 'Incididunt aliqua amet officia nostrud adipisicing velit fugiat ad qui ex anim anim ad. Labore occaecat do excepteur labore cillum sit ut mollit anim magna exercitation laboris et magna. Ad officia enim ipsum aliqua minim voluptate officia duis et adipisicing esse. Aliqua amet minim et dolore occaecat. Fugiat sint et ullamco laboris quis magna proident amet id velit nulla culpa. Id ullamco occaecat adipisicing consequat magna ea magna culpa culpa deserunt mollit velit eiusmod.\r\n',
-        preview: 'https://f1-intermix.netlify.app/',
-        github: 'https://github.com/CVE-078/f1-leaderboard',
-        type: 'Application',
-        stack: ['Vue.js', 'Firebase', 'Tailwind CSS', 'SCSS', 'BEM'],
-        picture: '/assets/hDC6DoO.webp'
-    }
+  {
+    id: 0,
+    name: 'Personal website',
+    description: 'Consectetur ullamco qui Lorem ad culpa sit do cupidatat nostrud duis ad et. Exercitation cupidatat consequat deserunt amet commodo sit velit labore amet mollit officia. Esse et quis irure sunt voluptate quis. Exercitation sunt eiusmod aute cillum proident sunt cillum. Labore adipisicing deserunt cillum dolor anim cupidatat in velit quis sunt esse id.\r\n',
+    preview: 'https://cve-personal.netlify.app/',
+    github: 'https://github.com/CVE-078/personal-website',
+    type: 'Website',
+    stack: ['React', 'SCSS', 'BEM'],
+    picture: '/assets/v7RfA1q.webp'
+  },
+  {
+    id: 1,
+    name: 'Airbnb Clone',
+    description: 'Dolore amet occaecat mollit mollit dolore. Et do anim est labore voluptate dolore nulla proident. Ipsum anim esse magna dolore quis ullamco consequat est ullamco nulla. Non eu consequat incididunt ut mollit pariatur irure officia. Eu commodo ad aliqua fugiat mollit amet.\r\n',
+    preview: 'https://airbnb-clone-react.vercel.app/',
+    github: 'https://github.com/CVE-078/airbnb-clone-react',
+    type: 'Clone',
+    stack: ['Next.js', 'Tailwind CSS', 'API'],
+    picture: '/assets/Wg58djZ.webp'
+  },
+  {
+    id: 2,
+    name: 'F1 Game Leaderboard',
+    description: 'Incididunt aliqua amet officia nostrud adipisicing velit fugiat ad qui ex anim anim ad. Labore occaecat do excepteur labore cillum sit ut mollit anim magna exercitation laboris et magna. Ad officia enim ipsum aliqua minim voluptate officia duis et adipisicing esse. Aliqua amet minim et dolore occaecat. Fugiat sint et ullamco laboris quis magna proident amet id velit nulla culpa. Id ullamco occaecat adipisicing consequat magna ea magna culpa culpa deserunt mollit velit eiusmod.\r\n',
+    preview: 'https://f1-intermix.netlify.app/',
+    github: 'https://github.com/CVE-078/f1-leaderboard',
+    type: 'Application',
+    stack: ['Vue.js', 'Firebase', 'Tailwind CSS', 'SCSS', 'BEM'],
+    picture: '/assets/hDC6DoO.webp'
+  }
 ]
 
 const jobs = [
-    {
-        id: 0,
-        company: 'Intermix',
-        url: 'https://www.intermix.nl/',
-        title: 'Web Developer',
-        location: 'Sliedrecht, Netherlands',
-        startDate: 'September 2019',
-        endDate: 'Present',
-        description: ''
-    },
-    {
-        id: 1,
-        company: 'Dutch Blue',
-        url: 'https://www.dutchblue.nl/',
-        title: 'Full Stack Developer',
-        location: 'Dordrecht, Netherlands',
-        startDate: 'March 2019',
-        endDate: 'August 2019',
-        description: ''
-    },
-    {
-        id: 2,
-        company: 'B.made',
-        url: 'https://www.bmade.nl/',
-        title: 'Service Developer',
-        location: 'Rotterdam, Netherlands',
-        startDate: 'August 2018',
-        endDate: 'February 2019',
-        description: ''
-    },
-    {
-        id: 3,
-        company: 'Intermix',
-        url: 'https://www.intermix.nl/',
-        title: 'Web Developer',
-        location: 'Sliedrecht, Netherlands',
-        startDate: 'March 2015',
-        endDate: 'July 2018',
-        description: ''
-    },
-    {
-        id: 4,
-        company: 'Intermix',
-        url: 'https://www.intermix.nl/',
-        title: 'Internship',
-        location: 'Sliedrecht, Netherlands',
-        startDate: 'September 2014',
-        endDate: 'February 2015',
-        description: ''
-    }
+  {
+    id: 0,
+    company: 'Software Bastards',
+    url: 'https://softwarebastards.nl/',
+    title: 'Front End Developer',
+    location: 'Abcoude, Netherlands',
+    startDate: 'November 2021',
+    endDate: 'Present',
+    description: ''
+  },
+  {
+    id: 1,
+    company: 'Intermix',
+    url: 'https://www.intermix.nl/',
+    title: 'Web Developer',
+    location: 'Sliedrecht, Netherlands',
+    startDate: 'September 2019',
+    endDate: 'October 2021',
+    description: ''
+  },
+  {
+    id: 2,
+    company: 'Dutch Blue',
+    url: 'https://www.dutchblue.nl/',
+    title: 'Full Stack Developer',
+    location: 'Dordrecht, Netherlands',
+    startDate: 'March 2019',
+    endDate: 'August 2019',
+    description: ''
+  },
+  {
+    id: 3,
+    company: 'B.made',
+    url: 'https://www.bmade.nl/',
+    title: 'Service Developer',
+    location: 'Rotterdam, Netherlands',
+    startDate: 'August 2018',
+    endDate: 'February 2019',
+    description: ''
+  },
+  {
+    id: 4,
+    company: 'Intermix',
+    url: 'https://www.intermix.nl/',
+    title: 'Web Developer',
+    location: 'Sliedrecht, Netherlands',
+    startDate: 'March 2015',
+    endDate: 'July 2018',
+    description: ''
+  },
+  {
+    id: 5,
+    company: 'Intermix',
+    url: 'https://www.intermix.nl/',
+    title: 'Internship',
+    location: 'Sliedrecht, Netherlands',
+    startDate: 'September 2014',
+    endDate: 'February 2015',
+    description: ''
+  }
 ]
 
 const menu = [
-    {
-        id: 0,
-        name: 'about',
-        link: '#about',
-        type: 'link'
-    },
-    {
-        id: 1,
-        name: 'jobs',
-        link: '#jobs',
-        type: 'link'
-    },
-    {
-        id: 2,
-        name: 'projects',
-        link: '#projects',
-        type: 'link'
-    },
-    {
-        id: 3,
-        name: 'say hello!',
-        link: '#contact',
-        type: 'button'
-    },
+  {
+    id: 0,
+    name: 'about',
+    link: '#about',
+    type: 'link'
+  },
+  {
+    id: 1,
+    name: 'jobs',
+    link: '#jobs',
+    type: 'link'
+  },
+  {
+    id: 2,
+    name: 'projects',
+    link: '#projects',
+    type: 'link'
+  },
+  {
+    id: 3,
+    name: 'say hello!',
+    link: '#contact',
+    type: 'button'
+  },
 ]
 
 const socials = [
-    {
-        id: 0,
-        name: 'LinkedIn',
-        url: 'https://www.linkedin.com/in/christiaan-van-eijnsbergen/',
-        icon: 'fab fa-fw fa-linkedin-in'
-    },
-    {
-        id: 1,
-        name: 'Github',
-        url: 'https://github.com/CVE-078',
-        icon: 'fab fa-fw fa-github'
-    }
+  {
+    id: 0,
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/christiaan-van-eijnsbergen/',
+    icon: 'fab fa-fw fa-linkedin-in'
+  },
+  {
+    id: 1,
+    name: 'Github',
+    url: 'https://github.com/CVE-078',
+    icon: 'fab fa-fw fa-github'
+  }
 ]
 
 const titles = ['a 27-year-old', 'an avid gamer', 'a problem solver', 'a father-of-one', 'a tv series binger', 'a web developer']
@@ -141,7 +151,7 @@ const about = `
     <p>My goals are to bring ideas and designs to life and create websites and applications users will have a great experience using.</p>
 
     <p>
-        I am currently employed at <a href="https://www.intermix.nl/" target="_blank" rel="noopener noreferrer" class="link link--external" alt="Intermix" title="Intermix">Intermix</a> as a Full-Stack Web Developer, developing and maintaining their custom-built Content Management System and also creating awesome new websites for customers.
+        I am currently employed at <a href="https://softwarebastards.nl/" target="_blank" rel="noopener noreferrer" class="link link--external" alt="Software Bastards" title="Software Bastards">Intermix</a> as a Front End Developer.
     </p>
 `
 
